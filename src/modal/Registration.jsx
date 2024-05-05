a0ed3b09e3d1eb6fd13f5af9b8de21c995e9544f
정명기 <jmk101711@naver.com>
2024-05-05 11:03:03 +0900

    feat: 로그인

--- src/modal/Registration.jsx
+++ src/modal/Registration.jsx
@@ -1,9 +1,17 @@
 import React, { forwardRef } from "react";
+import axios from "axios";
 import Input from "../component/basic/Input";
 import Button from "../component/basic/Button";
+import BaseUrl from "../util/BaseUrl";
 import "../styles/login.css";
 
 const Registration = forwardRef((props, ref) => {
+  const handleCompanySearch = (event) => {
+    const data = event.target.value;
+    axios.get(BaseUrl+`/api/v1/company/search`)
+
+  };
+
   return (
     <div id="register_container" ref={ref} onClick={props.onClick}>
       <div id="register_content">
