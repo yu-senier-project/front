6cf8390a143011dcd2bd4a8ea79b772a721333e3
Yeongi Park <yeongi0111@naver.com>
2024-05-09 16:57:48 +0900

    게시물 관련 api 함수 작성 완료

--- src/util/BaseUrl.jsx
+++ src/util/BaseUrl.jsx
@@ -1,3 +1,27 @@
-const baseUrl = "http://13.51.99.142:8080";
+// baseurl.jsx
+import axios from "axios";
 
-export default baseUrl;
+export const BaseUrl = "http://13.51.99.142:8080";
+
+const apiClient = axios.create({
+  baseURL: "http://13.51.99.142:8080",
+  headers: {
+    "Content-Type": "application/json",
+    // Authorization: "Bearer " + localStorage.getItem("accessToken"),
+  },
+  //   withCredentials: true,
+});
+apiClient.interceptors.request.use(
+  (config) => {
+    // 로컬 스토리지에서 토큰 가져오기
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default apiClient;
