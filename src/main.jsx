309f9358d972dada8cfb8b9ac644826fb45d4a89
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-05-10 22:35:11 +0900

    feat: 비밀번호 찾기 폼

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.scss";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    </React.StrictMode>
 );
--- src/store/find/useFindStore.jsx
+++ src/store/find/useFindStore.jsx
@@ -1,17 +1,41 @@
 // useFindStore.js
-import { create } from "zustand";
-import axios from "axios";
+import { create } from 'zustand';
+import axios from 'axios';
 
 const useFindStore = create((set) => ({
-  checkId: "",
-  setCheckId: async (email) => {
-    try {
-      const response = await axios.get(`http://13.51.99.142:8080/api/v1/auth/nickname-inquiry?nickname=${email}`);
-      set({ checkId: response.data.checkId }); // 응답 구조에 맞게 수정
-    } catch (error) {
-      console.error("Failed to fetch checkId:", error);
-    }
-  },
+    checkId: '',
+    firstName: '',
+    secondName: '',
+    email: '',
+    id: '',
+    isChecked: false,
+    //주어진 이메일로 ID를 확인하는 함수
+    setCheckId: async (email) => {
+        try {
+            const response = await axios.get(`http://13.51.99.142:8080/api/v1/auth/nickname-inquiry?nickname=${email}`);
+            set({ checkId: response.data.checkId });
+        } catch (error) {
+            console.error('Failed to fetch checkId:', error);
+        }
+    },
+
+    // 이름, 이메일 인증 모두 완료 했는가 확인하는 함수
+    setIsCheck: () =>
+        set((state) => ({
+            isChecked: !state.isChecked,
+        })),
+
+    // 이름을 설정하는 함수
+    setFirstName: (firstName) => set({ firstName }),
+
+    //성을 설정하는 함수
+    setSecondName: (secondName) => set({ secondName }),
+
+    // 이메일을 설정하는 함수
+    setEmail: (email) => set({ email }),
+
+    // ID를 설정하는 함수
+    setId: (id) => set({ id }),
 }));
 
 export default useFindStore;
