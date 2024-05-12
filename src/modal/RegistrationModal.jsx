eef53c5104643c06910a55acd3f5f7e8529dec3d
정명기 <jmk101711@naver.com>
2024-05-12 15:13:35 +0900

    feat: 회원가입 모달(수정중)

--- src/modal/RegistrationModal.jsx
+++ src/modal/RegistrationModal.jsx
@@ -1,41 +1,106 @@
-// RegistrationModal.jsx
-import React from 'react';
-import Modal from '@mui/material/Modal';
-import '../styles/registerModal.scss';
-import Input from '../component/basic/Input';
-import Button from '../component/basic/Button';
-import { useState } from 'react';
+import React, { useState, useCallback } from "react";
+import Modal from "@mui/material/Modal";
+import "../styles/registerModal.scss";
+import Input from "../component/basic/Input";
+import axios from "axios";
+import { debounce } from "lodash"; // lodash에서 debounce를 임포트합니다
+import Button from "../component/basic/Button";
 const style = {};
 
 function RegistrationModal({ open, handleClose }) {
-    const [formData, setFormData] = useState({
-        companyName: '',
-        firstName: '',
-        secondName: '',
-        userNickName: '',
-        password: '',
-        checkPassword: '',
-        email: '',
-        authCode: '',
-    });
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevState) => ({ ...prevState, [name]: value }));
-    };
+  const [formData, setFormData] = useState({
+    firstName: "",
+    secondName: "",
+    userNickName: "",
+    password: "",
+    checkPassword: "",
+    email: "",
+    authCode: "",
+  });
+  const [modalStep, setModalStep] = useState(false);
+  const [selectedCompany, setSelectedCompany] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [companys, setCompanys] = useState([]);
+  const [companyEmail, setCompanyEmail] = useState("");
+  const handleCompanyName = useCallback(
+    debounce((e) => {
+      const value = e.target.value;
+      setCompanyName(value);
+      if (value.trim()) {
+        axios
+          .get(
+            `http://13.51.99.142:8080/api/v1/company/search?keyword=${value}`
+          )
+          .then((response) => {
+            setCompanys(response.data);
+          })
+          .catch((error) => {
+            console.error("Error fetching companies:", error);
+          });
+      } else {
+        setCompanys([]);
+      }
+    }, 300),
+    []
+  );
 
-    return (
-        <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <div className="registermodal_contaniner">
-                <h1>가입하기</h1>
-                <Input size={'Big'} name={'companyName'} onChange={handleInputChange} />
-            </div>
-        </Modal>
-    );
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const getCompanyEmail = (companyName) => {
+    axios
+      .get(`http://13.51.99.142:8080/api/v1/company/${companyName}/email`)
+      .then((response) => {
+        console.log(response.data);
+        setCompanyEmail(response.data.email);
+        setSelectedCompany(companyName);
+      })
+      .catch((error) => {
+        console.error("Error fetching company email:", error);
+      });
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div className="register-modal">
+        <div className="header">
+          <h1>가입하기</h1>
+          <button className="close-button" onClick={handleClose}>
+            <h1>X</h1>
+          </button>
+        </div>
+
+        <Input size="Big" name="companyName" onChange={handleCompanyName} />
+        <div className="company-list">
+          {companys.length > 0 ? (
+            <ul>
+              {companys.map((company, index) => (
+                <li key={index}>
+                  {company.name}
+                  <button onClick={() => getCompanyEmail(company.name)}>
+                    선택
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : companyName.length > 0 ? (
+            <p>검색 결과가 없습니다.</p>
+          ) : (
+            <></>
+          )}
+        </div>
+        <p>선택된 회사:{selectedCompany}</p>
+        <Button text={"다음"} size={"Big"} />
+      </div>
+    </Modal>
+  );
 }
 
 export default RegistrationModal;
