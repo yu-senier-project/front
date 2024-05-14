bec881799d08ef96b7946fbb1e25dd9d1031b227
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-05-14 14:47:43 +0900

    Update MessageInviteModal.jsx

--- src/modal/MessageInviteModal.jsx
+++ src/modal/MessageInviteModal.jsx
@@ -13,15 +13,14 @@ function MessageInviteModal({ open, handleClose }) {
     const [people, setPeople] = useState([]);
     const [selectedPeople, setSelectedPeople] = useState([]);
     const [search, setSearch] = useState('');
-    const [newRoomName, setNewRoomName] = useState(localStorage.getItem("userNickName"));
-    const {addRoom , fetchRooms} = useMessageStore();
-    // debounce 설정 부분 수정
+    const [newRoomName, setNewRoomName] = useState(localStorage.getItem("userNickName") || '');
+    const { addRoom, fetchRooms } = useMessageStore();
+
     const debouncedFetchPeople = useCallback(debounce(async (value) => {
         if (value.trim()) {
             const memberId = localStorage.getItem("memberId");
             try {
                 const { data } = await apiClient.get(`/api/v1/member/only-company/search?memberId=${memberId}&nickname=${value}`);
-                console.log(data);
                 setPeople(data);
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -33,16 +32,17 @@ function MessageInviteModal({ open, handleClose }) {
 
     const handleSearchChange = (e) => {
         const value = e.target.value;
-        setSearch(value);  // 즉각적인 상태 업데이트
-        debouncedFetchPeople(value);  // debounce 적용된 검색 함수 호출
+        setSearch(value);
+        debouncedFetchPeople(value);
     };
 
-    const handleSelectPerson = (nickname) => {
-        setSelectedPeople(prev => {
-            if (prev.includes(nickname)) {
-                return prev.filter(n => n !== nickname);
+    const handleSelectPerson = (nickname, memberId) => {
+        setSelectedPeople((prev) => {
+            const isSelected = prev.some((person) => person.memberId === memberId);
+            if (isSelected) {
+                return prev.filter((person) => person.memberId !== memberId);
             } else {
-                return [...prev, nickname];
+                return [...prev, { nickname, memberId }];
             }
         });
     };
@@ -51,66 +51,71 @@ function MessageInviteModal({ open, handleClose }) {
         setNewRoomName(e.target.value);
     };
 
+    const handleRoomCreation = () => {
+        addRoom(newRoomName, selectedPeople);
+        fetchRooms(localStorage.getItem("memberId"), 1);
+        setModalStep(0);
+        setPeople([]);
+        setSelectedPeople([]);
+        handleClose();
+    };
+
+    const renderPeopleList = () => {
+        if (people.length > 0) {
+            return (
+                <ul>
+                    {people.map((person) => (
+                        <li key={person.memberId}>
+                            {person.nickname}
+                            <button
+                                onClick={() => handleSelectPerson(person.nickname, person.memberId)}
+                                className={selectedPeople.some((selected) => selected.memberId === person.memberId) ? 'button-selected' : ''}
+                            >
+                                선택
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            );
+        } else if (search.length > 0) {
+            return <p>검색 결과가 없습니다.</p>;
+        }
+        return null;
+    };
+
     const renderModalContent = () => {
-        switch (modalStep) {
-            case 0:
-                return (
-                    <div className="addroom-modal">
-                        <div className="header">
-                            <button className="close-button" onClick={handleClose}>
-                                <CloseButton />
-                            </button>
-                        </div>
-                        <Input size="Large" name="invite" value={search} onChange={handleSearchChange} />
-                        <div className="people-list">
-                            {people.length > 0 ? (
-                               <ul>
-                               {people.map((person, index) => (
-                                   <li key={index}>
-                                       {person.nickname}
-                                       <button 
-                                           onClick={() => handleSelectPerson(person.memberId)}
-                                           className={selectedPeople.includes(person.memberId) ? 'button-selected' : ''}
-                                       >
-                                           선택
-                                       </button>
-                                   </li>
-                               ))}
-                           </ul>
-                            ) : search.length > 0 ? (
-                                <p>검색 결과가 없습니다.</p>
-                            ) : (
-                                <></>
-                            )}
-                        </div>
-                        <Button text="다음" size="Large" onClick={() => setModalStep(1)} />
+        if (modalStep === 0) {
+            return (
+                <div className="addroom-modal">
+                    <div className="header">
+                        <button className="close-button" onClick={handleClose}>
+                            <CloseButton />
+                        </button>
                     </div>
-                );
-            case 1:
-                return (
-                    <div className="addroom-modal">
-                        <div className="header">
-                            <button className="close-button" onClick={() => setModalStep(0)}><h1>←</h1></button>
-                            <button className="close-button" onClick={handleClose}>
-                                <CloseButton />
-                            </button>
-                        </div>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                        <p>사용하실 채팅방의 이름을 적어주세요</p>
-                        <p>미 작성시 채팅방 회원의 이름으로 작성됩니다</p>
-                        <br></br>
-                    <br></br>
-                    <br></br>
-                        <Input size="Large" name="newRoomName" value={newRoomName} onChange={handleNewRoomNameChange} />
-                        <Button text="채팅방 생성" size="Large" onClick={()=>{addRoom(newRoomName,selectedPeople); fetchRooms(localStorage.getItem("memberId"),1); handleClose()}} />
+                    <Input size="Large" name="invite" value={search} onChange={handleSearchChange} />
+                    <div className="people-list">
+                        {renderPeopleList()}
                     </div>
-                );
-            default:
-                return <p>초기화 오류</p>;
+                    <Button text="다음" size="Large" onClick={() => setModalStep(1)} />
+                </div>
+            );
+        } else if (modalStep === 1) {
+            return (
+                <div className="addroom-modal">
+                    <div className="header">
+                        <button className="close-button" onClick={() => setModalStep(0)}><h1>←</h1></button>
+                        <button className="close-button" onClick={handleClose}>
+                            <CloseButton />
+                        </button>
+                    </div>
+                    <p>사용하실 채팅방의 이름을 적어주세요</p>
+                    <p>미 작성시 채팅방 회원의 이름으로 작성됩니다</p>
+                    <Input size="Large" name="newRoomName" value={newRoomName} onChange={handleNewRoomNameChange} />
+                    <Button text="채팅방 생성" size="Large" onClick={handleRoomCreation} />
+                </div>
+            );
+        } else {
+            return <p>초기화 오류</p>;
         }
     };
 
