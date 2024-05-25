cdec6901bfdc2eb8da8b7a1e5632772cbd64d9ba
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-05-25 21:20:15 +0900

    feat: 필요없는 버튼 삭제

--- src/pages/project/ProjectGantt.jsx
+++ src/pages/project/ProjectGantt.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import "dhtmlx-gantt/codebase/dhtmlxgantt.css";
 import gantt from "dhtmlx-gantt";
+import { useSearchParams } from "react-router-dom";
 import useProjectStore from "../../store/project/useProjectStore";
 import { useGetPlanList } from "../../react-query/useProject";
 import { parseDate } from "../../util/parseDate";
+import { CreateSchedule } from "../../component/project/CreateSchedule";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 import "../../styles/project/GanttChart.scss";
 
 export default function ProjectGantt() {
@@ -12,6 +15,8 @@ export default function ProjectGantt() {
   const { projectId } = useParams();
   const { data, isLoading } = useGetPlanList(projectId);
   const [tasks, setTasks] = useState({ data: [], links: [] });
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [title, setTitle] = useState(searchParams.get("title"));
   const ganttContainer = useRef(null);
 
   useEffect(() => {
@@ -55,6 +60,11 @@ export default function ProjectGantt() {
       gantt.config.show_progress = false; // 진행도 바 제거
       gantt.config.show_links = false; // 링크 아이콘 제거
 
+
+      gantt.config.drag_resize = false; //일정 늘리기 제거
+      gantt.config.drag_progress = false; // 
+      gantt.config.drag_move = false;
+
       gantt.templates.timeline_cell_class = function (task, date) {
         if (date.getDay() === 0 || date.getDay() === 6) {
           // 주말
@@ -77,7 +87,7 @@ export default function ProjectGantt() {
       };
 
       gantt.templates.task_class = function (start, end, task) {
-        return task.color; // 각 태스크에 대한 사용자 정의 CSS 클래스 할당
+        return task.color; 
       };
 
       gantt.render();
@@ -85,19 +95,36 @@ export default function ProjectGantt() {
   }, [tasks]);
 
   return (
-    <div>
-      <div
+    <div className="gantt-chart">
+      <div className="ProjectCalendar-top">
+        <h2>
+          <button>
+            <HiOutlineDotsVertical size={20} />
+          </button>
+          {title}
+        </h2>
+        <div>
+          <button
+            onClick={() => {
+              setOnCreate(true);
+            }}
+          >
+            새 일정 만들기
+          </button>
+        </div>
+      </div>
+      <div className="gantt"
         ref={ganttContainer}
         id="gantt_here"
-        style={{ width: "100%", height: "500px" }}
+        style={{ }}
       ></div>
-      <button
+      {/* <button
         onClick={() => {
           console.log(tasks);
         }}
       >
         asdfasfasdfasdfasdfasfd
-      </button>
+      </button> */}
     </div>
   );
 }
