901710c6d7b5dbf7c49bfcea579d902f117da408
Yeongi Park <yeongi0111@naver.com>
2024-05-31 21:04:46 +0900

    url 변경하고 빌드

--- src/util/BaseUrl.jsx
+++ src/util/BaseUrl.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const BaseUrl = "http://13.51.99.142:8080";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:80",
+  baseURL: "http://43.203.69.159:80",
   headers: {
     "Content-Type": "application/json",
   },
