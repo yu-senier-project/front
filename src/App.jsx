1c892a5807a275e0be5f66acd7520bdcb22784e2
Yeongi Park <yeongi0111@naver.com>
2024-06-03 17:11:02 +0900

    급히 수정

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@ function App() {
           </Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
+        <ReactQueryDevtools buttonPosition="bottom"> </ReactQueryDevtools>
       </BrowserRouter>
     </>
   );
