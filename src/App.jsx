cb3ee01261dfcf14a23cd19984460fdea228ff12
jungmyunggi <143400940+jungmyunggi@users.noreply.github.com>
2024-06-06 08:26:58 +0900

    fix: 비밀번호 재설정 로직 변경

--- src/App.jsx
+++ src/App.jsx
@@ -41,12 +41,12 @@ function App() {
         {toggle && <CreateFeed />}
         <Routes>
           <Route path="/" element={<Login />} />
-
-          <Route path="/" element={<SmallNav />}>
-            <Route path="Id" element={<IdFind />} />
+          <Route path="Id" element={<IdFind />} />
             <Route path="CheckId" element={<IdCheck />} />
             <Route path="Password" element={<PwInitCheckId />} />
             <Route path="PasswordInit" element={<PwInit />} />
+          <Route path="/" element={<SmallNav />}>
+           
             <Route path="Home" element={<Home />} />
             <Route path="Search" element={<Search />} />
             <Route path="Project" element={<ProjectHome />} />
